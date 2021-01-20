4ce4b2ba56918dc310097ca792e40685a57a4816
Sam Schott <ss2151@cam.ac.uk>
2021-01-20 16:52:01 +0000

    Bump version: 1.1.2.dev0 → 1.1.2

--- src/desktop_notifier/__init__.py
+++ src/desktop_notifier/__init__.py
@@ -2,6 +2,6 @@
 from .main import DesktopNotifier, Notification, NotificationLevel
 
 
-__version__ = "1.1.2.dev0"
+__version__ = "1.1.2"
 __author__ = "Sam Schott"
 __url__ = "https://github.com/samschott/desktop-notifier"
