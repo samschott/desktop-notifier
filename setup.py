a77e93816b967b9252b5490a237185e0b0d04745
Sam Schott <ss2151@cam.ac.uk>
2021-02-07 12:46:48 +0000

    Bump version: 1.2.0.dev0 → 1.2.0

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     url="https://github.com/samschott/desktop-notifier",
-    version="1.2.0.dev0",
+    version="1.2.0",
     zip_safe=False,
 )
