8c43f280d1be2a3d5a4eebf000c6e1ff94a6e423
Sam Schott <ss2151@cam.ac.uk>
2021-02-10 16:52:52 +0000

    Bump version: 2.0.2 → 2.0.3

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     url="https://github.com/samschott/desktop-notifier",
-    version="2.0.2",
+    version="2.0.3",
     zip_safe=False,
 )
--- src/desktop_notifier/__init__.py
+++ src/desktop_notifier/__init__.py
@@ -2,6 +2,6 @@
 from .main import DesktopNotifier, Notification, NotificationLevel
 
 
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 __author__ = "Sam Schott"
 __url__ = "https://github.com/samschott/desktop-notifier"
