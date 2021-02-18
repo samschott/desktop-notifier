0912eed405f02e5a3b3bb9c01b33ed194b38ff5d
Sam Schott <ss2151@cam.ac.uk>
2021-02-18 11:20:46 +0000

    Bump version: 3.2.0 → 3.2.1

--- src/desktop_notifier/__init__.py
+++ src/desktop_notifier/__init__.py
@@ -2,6 +2,6 @@
 from .main import DesktopNotifier, Button, ReplyField, Notification, Urgency
 
 
-__version__ = "3.2.0"
+__version__ = "3.2.1"
 __author__ = "Sam Schott"
 __url__ = "https://github.com/samschott/desktop-notifier"
