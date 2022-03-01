f3576b7cf7ce958f00d727942b1c8c06cd0e68b4
samschott <sam.schott@outlook.com>
2022-03-01 21:57:45 +0100

    Bump version: 3.3.4.dev0 → 3.3.4

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 # -- Project information ---------------------------------------------------------------
 
 author = "Sam Schott"
-version = "3.3.4.dev0"
+version = "3.3.4"
 release = version
 project = "dekstop-notifier"
 title = "Desktop-Notifier Documentation"
