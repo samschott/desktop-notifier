83f71b4c6c589206bdc592dff2beeb8daab7c467
samschott <sam.schott@outlook.com>
2022-11-21 00:16:19 +0100

    explicit export from __init__.py

--- src/desktop_notifier/__init__.py
+++ src/desktop_notifier/__init__.py
@@ -5,3 +5,14 @@ from .main import DesktopNotifier, Button, ReplyField, Notification, Urgency
 __version__ = "3.4.2.dev1"
 __author__ = "Sam Schott"
 __url__ = "https://github.com/samschott/desktop-notifier"
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__url__",
+    "Notification",
+    "Button",
+    "ReplyField",
+    "Urgency",
+    "DesktopNotifier",
+]
