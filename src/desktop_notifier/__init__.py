00fa0009949659edd573bcd570f76cd5281f07fd
samschott <sam.schott@outlook.com>
2023-07-10 22:33:24 +0200

    Bump version: 3.5.4 → 3.5.5.dev0

--- src/desktop_notifier/__init__.py
+++ src/desktop_notifier/__init__.py
@@ -2,7 +2,7 @@
 from .main import DesktopNotifier, Button, ReplyField, Notification, Urgency
 
 
-__version__ = "3.5.4"
+__version__ = "3.5.5.dev0"
 __author__ = "Sam Schott"
 __url__ = "https://github.com/samschott/desktop-notifier"
 
