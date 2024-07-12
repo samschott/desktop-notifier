11c7df34800b9c09236c5d1fac276ed24d39411a
ashariyar <ashariyar@users.noreply.github.com>
2024-07-12 19:54:53 -0400

    linter

--- tests/test_notification.py
+++ tests/test_notification.py
@@ -1,5 +1,7 @@
-CATEGORY_ID = "desktop-notifier__button-title-Mark as read" \
-              "__reply-title-And Cassius, too?__reply-button-title-Send it"
+CATEGORY_ID = (
+    "desktop-notifier__button-title-Mark as read"
+    "__reply-title-And Cassius, too?__reply-button-title-Send it"
+)
 
 
 def test_category_id(notification):
