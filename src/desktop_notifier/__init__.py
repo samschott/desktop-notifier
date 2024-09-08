55d6028268c0cee6bfb6b87917c42623266196fc
Sam Schott <sam.schott@outlook.com>
2024-09-08 22:14:00 +0200

    Bump version: 6.0.0.dev0 → 6.0.0

--- src/desktop_notifier/__init__.py
+++ src/desktop_notifier/__init__.py
@@ -17,7 +17,7 @@ from .main import (
 )
 from .sync import DesktopNotifierSync
 
-__version__ = "6.0.0.dev0"
+__version__ = "6.0.0"
 __author__ = "Sam Schott"
 __url__ = "https://github.com/samschott/desktop-notifier"
 
