1c1f873287b395cf5364b9fe436a21cd66a92a90
samschott <sam.schott@outlook.com>
2025-04-27 21:09:26 +0200

    Bump version: 6.1.1.dev0 → 6.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 # -- Project information ---------------------------------------------------------------
 
 author = "Sam Schott"
-version = "6.1.1.dev0"
+version = "6.1.1"
 release = version
 project = "desktop-notifier"
 title = "Desktop-Notifier Documentation"
